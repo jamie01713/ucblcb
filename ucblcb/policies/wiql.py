ab467a4192b9a872a45c1f189fcd19b74cc2851d
Ivan Nazarov <ivannz@yandex.ru>
2025-01-25 16:38:39 +0100

    cleaned up docs, made final checks on the q-learning impl

--- ucblcb/policies/wiql.py
+++ ucblcb/policies/wiql.py
@@ -37,15 +37,9 @@ class WIQL(BasePolicy):
       is to select M arms. Our action-selection method ensures that an arm
       whose estimated benefit is higher is more likely to get selected.
 
-    Therefore, WIQL maintains an independet Q-function approximation for each arm
+    Therefore, WIQL maintains an independent Q-function approximation for each arm
     and learns them based on their individual :math:`(s_t, a_t, r_{t+1}, s_{t+1})`
     transitions.
-
-    In deep Q-learning methods the Q-function is approximated by a neural network, and
-    to avoid the convergence issues due to appxorimation of the backup, a secondary
-    "frozen" target Q-function is introduced to estimate :math:`\hat{V}(s_{t+1})`.
-    In small discrete state-action space setting it is tractable to use an exact
-    parameterization of the q-function, i.e. as a table indexed by states and actions.
 
     References
     ----------
@@ -65,7 +59,7 @@ class WIQL(BasePolicy):
     # \hat{q}_{aks}
     #     \approx E_{p_k(x|s, a)} r_{kasx} + \gamma \max_a q_{akx}
     qval_aks_: ndarray[float]  # (A, N, S)
-    n_pulls_aks_: ndarray[int]  # (A, N, S)
+    n_aks_: ndarray[int]  # (A, N, S)
 
     def __repr__(self) -> str:
         return type(self).__name__ + f"(alpha={self.alpha}, gamma={self.gamma})"
@@ -85,12 +79,10 @@ class WIQL(BasePolicy):
         super().__init__(n_max_steps, budget, n_states)
 
         assert isinstance(alpha, float) and 0 <= alpha <= 1
-        self.alpha = alpha
-
         assert isinstance(gamma, float) and 0 <= gamma < 1
-        self.gamma = gamma
-
         assert isinstance(epsilon, float) and 0 <= epsilon < 1
+        self.alpha = alpha
+        self.gamma = gamma
         self.epsilon = epsilon
 
     def setup_impl(self, /, obs, act, rew, new, *, random: Generator = None):
@@ -99,41 +91,51 @@ class WIQL(BasePolicy):
 
         # init the arm-state-action value table
         shape = self.n_actions, self.n_arms_in_, self.n_states
-        self.qval_aks_ = np.zeros(shape, float)
-        self.n_pulls_aks_ = np.zeros(shape, int)
-        # self.qval_aks_ = random.normal(size=shape)
-
-        # the expected subsidy
-        self.lam_ks_ = np.zeros(shape[1:], float)
+        self.qval_aks_ = np.zeros(shape, float)  # random.normal(size=shape)
+        self.n_aks_ = np.zeros(shape, int)
 
         return self
 
     def update_impl(self, /, obs, act, rew, new, *, random: Generator = None):
+        """Update the q-function estimate on the batch of transitions."""
+
+        # let the base class update the its state
         super().update_impl(obs, act, rew, new, random=random)
 
-        # update of the arm-state-actionq-value tables
+        # update of the arm-state-action q-value tables
         idx = np.broadcast_to(np.arange(self.n_arms_in_)[np.newaxis], obs.shape)
 
-        # m_{aks} = \sum_{j: x^k_j = s, a^k_j = a} 1
-        m_aks = np.zeros_like(self.n_pulls_aks_, int)
+        # `m_{aks}` the number of sample which had action `a` applied to arm `k`
+        #  in state `s`
+        # XXX the `obs-act-rew-new` arrays have shape `(batch, N)`
+        # XXX strictly speaking in RL terms, our env has vector state space (vector
+        #  `s \in S^N` indicating state `s_k` of arm `k`) and vector action space
+        #  where `a \in [A]^N` is a vector of control signals applied to each arm:
+        #  `a_k` tells us what was issued to the `k`-th arm.
+        m_aks = np.zeros_like(self.n_aks_, int)
         np.add.at(m_aks, (act, idx, obs), 1)
-        self.n_pulls_aks_ += m_aks
+        self.n_aks_ += m_aks
 
-        # compute `\delta^0_{kj} = r_{kj} + \gamma v_{k x_j} - q_{k s_j a_j}`, where
-        #  `v_{ks} = \max_u q_{ksu}`, for all transitions `j` (we do have j+1 here,
-        #  since we have agreed that `s, a -->> r, x`)
-        # gam = np.where(fin > 0, 0.0, self.gamma)  # fin: +1 terminated, -1 truncated
-        gam = self.gamma
-        val_star = np.max(self.qval_aks_, axis=0)[idx, new]  # (..., N)
-        td0 = rew + gam * val_star - self.qval_aks_[act, idx, obs]  # (..., N)
+        # compute the temporal-difference target for the observed SARX data and each arm
+        #  `\delta^0_{jk} = r_{jk} + \gamma \max_u q_k(x_{jk}, u)`
+        #  for transition `s_j, a_j \to r_j, x_j` from the batch (origin state `s_j`
+        #  consequence state `x_j`).
+        td_jk = rew + self.gamma * np.max(self.qval_aks_, axis=0)[idx, new]
 
-        # get the sum of errors at each arm-state-action
-        td0_aks = np.zeros_like(self.qval_aks_)
-        np.add.at(td0_aks, (act, idx, obs), td0)
+        # get the gradient of the td-error ell-2 loss:
+        #   \frac1J \sum_{jk} \frac12 (\delta^0_{jk} - q_k(s_{jk}, a_{jk}))^2
+        #  the gradient wrt `q_k` as a function is
+        #   g: (s, a)
+        #       \mapsto \frac1J \sum_{j \colon s_{jk} = s, a_{jk} = a}
+        #           (q_k(s_{jk}, a_{jk}) - \delta^0_{jk})
+        grad_aks = np.zeros_like(self.qval_aks_)
+        qval_jk = self.qval_aks_[act, idx, obs]
+        np.add.at(grad_aks, (act, idx, obs), qval_jk - td_jk)
+        np.divide(grad_aks, m_aks, where=m_aks > 0, out=grad_aks)
 
         # make a step in direction of reducing the td(0) error
-        np.divide(td0_aks, m_aks, where=m_aks > 0, out=td0_aks)
-        self.qval_aks_ += self.alpha * td0_aks
+        # functional gradient descent: q \lefthookarrow q + \alpha \nabla_q L(q)
+        self.qval_aks_ -= self.alpha * grad_aks
 
         return self
 
@@ -141,27 +143,34 @@ class WIQL(BasePolicy):
 
     def decide_impl(self, random: Generator, /, obs):
         """Decide the action to play to each arm at the provided observed state."""
+
         # `obs` is (batch, n_arms) and, if initialized, `n_arms == .n_arms_in_`
         # XXX usually `batch == 1`, i.e. single-element batch of observations.
         idx = np.broadcast_to(np.arange(self.n_arms_in_)[np.newaxis], obs.shape)
 
-        # the estimated subsidy for not playing `a=1` for arm `k` in state `s`
-        m_ak = self.n_pulls_aks_[:, idx, obs]  # (A, ..., N)
-        q_ak = self.qval_aks_[:, idx, obs]  # (A, ..., N)
-        q_0k = np.where(m_ak[0] > 0, q_ak[0], -np.inf)  # (..., N)
-        q_1k = np.where(m_ak[1] > 0, q_ak[1], np.inf)  # (..., N)
-        lam_order = np.argsort(q_1k - q_0k, -1)  # (..., N)
-
         # get the number of arms in multi-arm 2d+ data
-        n_samples, n_arms, *_ = np.shape(obs)
+        n_samples, n_arms, *_ = np.shape(obs)  # XXX `...` below is `n_samples`
 
-        # sample random subsets for each sample and then pick which action to play
-        rnd_subset = random_subset(random, n_arms, self.budget, size=n_samples)
+        # the estimated subsidy for not playing `a=1` for arm `k` in state `s`
+        m_ak = self.n_aks_[:, idx, obs]  # (A, ..., N)
+        q_ak = self.qval_aks_[:, idx, obs]  # (A, ..., N)
 
-        # get the binary interaction mask (integers)
-        lam_subset = np.zeros(lam_order.shape, int)
-        np.put_along_axis(lam_subset, lam_order[..., -self.budget :], 1, axis=-1)
+        # if an arm `k` has never been acted upon by `a` in state `s`, then the
+        #  q-value estimate `q_k(s, a)` is undefined.
+        q_0k = np.where(m_ak[0] > 0, q_ak[0], -np.inf)
+        q_1k = np.where(m_ak[1] > 0, q_ak[1], np.inf)
 
-        # pick which samples are to be played with randomly
+        # prepare action vector using on the estimated advantages of playing `a_k = 1`
+        #  for arm `k` in its current state `s_k`: the higher the advantage, then
+        #  more reason to pull. Whittle index's subsidy `\lambda_k` is, roughly,
+        #     q_k(s_k, a=1) - \lambda_k \approx q_k(s, a=0) \,.
+        subset_lam = np.zeros((n_samples, n_arms), int)
+        top_k = np.argsort(q_1k - q_0k, -1)[..., -self.budget :]  # (..., N)
+        np.put_along_axis(subset_lam, top_k, 1, axis=-1)
+
+        # draw a random subset for each sample and then pick which action to play
+        #  with probability `self.epsilon` (for \epsilon-greedy)
+        subset_rnd = random_subset(random, n_arms, self.budget, size=n_samples)
+
         is_random = random.uniform(size=(n_samples, 1)) < self.epsilon
-        return np.where(is_random, rnd_subset, lam_subset)
+        return np.where(is_random, subset_rnd, subset_lam)
