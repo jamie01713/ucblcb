8423d2230eba70e12a54a9f8982c1cb1b6c24824
Ivan Nazarov <ivannz@yandex.ru>
2025-02-14 01:08:32 +0100

    stop exportin policies from dunder-init

--- ucblcb/policies/__init__.py
+++ ucblcb/policies/__init__.py
@@ -1,8 +1,3 @@
 # flake8: noqa: F401
 
 from .base import RandomSubsetPolicy
-from .lcbggt import LGGT
-from .whittle import Whittle
-from .wiql import WIQL, WIQLPriv
-from .ucw import UCWhittleExtreme, UCWhittleUCB
-from .ucw import UCWhittlePv, UCWhittlePvPriv, UCWhittleUCBPriv
